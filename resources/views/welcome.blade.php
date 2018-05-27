61be73b71286a71da5cbdea7561cb48826ca7efc
noufi <noufi@ascdynsvr2.com>
2018-05-27 11:00:47 +0400

    fourth commit in master branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test 2
+                    Test 3
                 </div>
 
                 <div class="links">
