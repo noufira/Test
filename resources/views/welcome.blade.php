ac0cd8941b7ca82c5cbd2e334094e7281369e861
noufi <noufi@ascdynsvr2.com>
2018-05-27 10:58:18 +0400

    Third commit in master branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test
+                    Test 2
                 </div>
 
                 <div class="links">
